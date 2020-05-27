b3c2480dedc871bb6caa94850126883f3cfe4c8d
wangdong <a13915078613@163.com>
2020-05-27 14:57:54 +0800

    first commit

---
+++ docs/precache-manifest.18e051be0f0bd93f4daf99d3020c77af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3809d8ac24608038a32fe7202ff9b21a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b0d702c96ca797bca73e",
+    "url": "./static/css/2.e21a5cdd.chunk.css"
+  },
+  {
+    "revision": "63dad887d6a41ada6543",
+    "url": "./static/css/main.56c99c49.chunk.css"
+  },
+  {
+    "revision": "b0d702c96ca797bca73e",
+    "url": "./static/js/2.6a61d727.chunk.js"
+  },
+  {
+    "revision": "63dad887d6a41ada6543",
+    "url": "./static/js/main.bdcc18d9.chunk.js"
+  },
+  {
+    "revision": "c7ba62c9eb2c1a9f7005",
+    "url": "./static/js/runtime~main.bccd126a.js"
+  },
+  {
+    "revision": "b06e2f41892cf78358da532c3079c5e4",
+    "url": "./static/media/head.b06e2f41.png"
+  }
+]);
